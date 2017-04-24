79df3aded424e28e2b2854f6796c2636203dabc1
Adhy <adhywiranatap@gmail.com>
2017-04-24 21:17:10 +0700

    added about container

---
+++ src/Containers/AboutContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AboutContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
+
+export default AboutContainer;
