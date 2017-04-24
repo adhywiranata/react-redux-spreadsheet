1641cb1081ada771e03d6b60e08663adba7d0ebb
Adhy <adhywiranatap@gmail.com>
2017-04-24 14:14:57 +0700

    added Chart Container

---
+++ src/Containers/ChartContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChartContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Charts</h2>
+      </div>
+    );
+  }
+}
+
+export default ChartContainer;
