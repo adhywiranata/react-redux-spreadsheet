ebedf36eb715b0b5576e79d26d43f4de99159505
Adhy <adhywiranatap@gmail.com>
2017-04-24 21:36:05 +0700

    updated about page

--- src/Containers/AboutContainer.js
+++ src/Containers/AboutContainer.js
@@ -6,8 +6,8 @@ class AboutContainer extends Component {
     return (
       <div>
         <h2>About</h2>
-        <p>This is a simple spreadsheet using React and Redux(not yet). Made by
-          <a href="https://github.com/adhywiranata" target="_blank">Adhy Wiranata</a>
+        <p>This is a simple spreadsheet using React and Redux(not yet).<br/><br/><br/>
+          Made by <a href="https://github.com/adhywiranata" target="_blank">Adhy Wiranata</a>
         </p>
       </div>
     );
