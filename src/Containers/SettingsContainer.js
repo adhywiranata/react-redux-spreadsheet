9461998310e00f0608629f76e1041d975f6cb90b
Adhy <adhywiranatap@gmail.com>
2017-04-24 21:17:23 +0700

    added settings container

---
+++ src/Containers/SettingsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SettingsContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Settings (Coming Soon)</h2>
+      </div>
+    );
+  }
+}
+
+export default SettingsContainer;
