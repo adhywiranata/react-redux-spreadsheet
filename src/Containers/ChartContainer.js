4b50fd33728e176b53b84cffa5bcf5397d30b9e2
Adhy <adhywiranatap@gmail.com>
2017-05-04 07:48:34 +0700

    updated chart fluid size

--- src/Containers/ChartContainer.js
+++ src/Containers/ChartContainer.js
@@ -21,13 +21,13 @@ class ChartContainer extends Component {
           .append('svg')
           .style('width', '100%')
           .style('height', '100%')
-         //responsive SVG needs these 2 attributes and no width and height attr
-          .attr('preserveAspectRatio', 'xMinYMin meet')
+        //  //responsive SVG needs these 2 attributes and no width and height attr
+        //   .attr('preserveAspectRatio', 'xMinYMin meet')
           .style('padding', 50)
           .style('background-color', '#FFFFFF');
 
-    var svgWidth = parseInt(svg.style('width').substring(0, 3), 10) * 10;
-    var svgHeight = parseInt(svg.style('height').substring(0, 3), 10);
+    var svgWidth = parseInt(svg.style('width').replace('px', ''), 10);
+    var svgHeight = parseInt(svg.style('height').replace('px', ''), 10);
 
     var yScale = d3.scaleLinear()
               .domain([0, d3.max(dataset)])
@@ -94,19 +94,18 @@ class ChartContainer extends Component {
     console.log(nextState.dataset);
 
     const { dataset } = nextState;
-    // const barPadding = 5;
 
     // reselect the svg
     var svg = d3.select('#SvgWrapper');
 
-    var svgWidth = parseInt(svg.style('width').substring(0, 3), 10) * 10;
-    var svgHeight = parseInt(svg.style('height').substring(0, 3), 10);
+    var svgWidth = parseInt(svg.style('width').replace('px', ''), 10);
+    var svgHeight = parseInt(svg.style('height').replace('px', ''), 10);
 
     // recalculate the scales
     var yScale = d3.scaleLinear()
               .domain([0, d3.max(dataset)])
               .range([svgHeight, 0]);
-    //
+
     var xScale = d3.scaleLinear()
               .domain([0, dataset.length])
               .range([0, svgWidth]);
@@ -139,25 +138,6 @@ class ChartContainer extends Component {
         .attr('height', svgHeight - yScale(val))
         .attr('y', yScale(val));
     });
-
-    // svg.selectAll('.bar')
-    //   .data(dataset)
-    //   .enter()
-    //     .append('rect')
-    //     .attr('height', 10)
-    //     .attr('width', 10)
-    //     .attr('fill', function(d) {
-    //      return '#1BBC9B';
-    //     })
-        // .transition()
-        //   .duration(500)
-        //   .ease(d3.easeElastic)
-        //   .attr('height', function(d) {
-        //      return svgHeight - yScale(dataset[0]);
-        //   })
-        //   .attr('y', function(d) {
-        //      return svgHeight - 10;
-        //   })
   }
 
   render() {
@@ -167,7 +147,12 @@ class ChartContainer extends Component {
         <button onClick={this.changeDataset} style={{ alignSelf: 'center', width: '50%', padding: 10, backgroundColor: 'teal', color: 'white', border: '0', margin: 10 }}>
           Change Value
         </button>
-        <div id="SvgWrapper" style={{ width: '70%', height: 300, margin: '0 auto' }}></div>
+        <div style={{ display: 'flex', flexDirection: 'row', padding: 20 }}>
+          <div style={{ width: '50%', backgroundColor: 'teal', height: 500 }}></div>
+          <div style={{ width: '40%', height: 500,  }}>
+            <div id="SvgWrapper" style={{ width: '100%', height: 300, margin: '0 auto', padding: 0 }}></div>
+          </div>
+        </div>
       </div>
     );
   }
